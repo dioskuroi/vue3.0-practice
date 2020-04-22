5be41de0febc6f22474913777a880cbafec83f46
徐俊 <xujn1019@hotmail.com>
2020-04-22 16:20:18 +0800

    feat: common type

---
+++ src/index.d.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: xujun
+ * @Date: 2020-04-22 11:05:21
+ * @LastEditTime: 2020-04-22 11:06:19
+ * @LastEditors: xujun
+ * @FilePath: /vue-next-practice/src/index.d.ts
+ * @Description: 通用类型
+ */
+
+ declare type BaseElement = HTMLBaseElement | null
