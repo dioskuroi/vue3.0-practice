51c9b05280832820b1eb36edfedb5c2910e778e9
徐俊 <xujn1019@hotmail.com>
2020-05-07 11:17:09 +0800

    feat: tsconfig

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: xujun
  * @Date: 2020-04-17 11:16:33
- * @LastEditTime: 2020-04-23 10:19:28
+ * @LastEditTime: 2020-05-07 09:12:56
  * @LastEditors: xujun
  * @FilePath: /vue-next-practice/src/main.ts
  * @Description: 项目入口文件
@@ -12,13 +12,13 @@ import App from './App.vue'
 
 import router from './router'
 
-import fastclick from 'fastclick'
+import Fastclick from 'fastclick'
 import { lazyLoad } from './plugins/lazy-load'
 import 'common/stylus/index.styl'
 
-fastclick.attach(document.body)
+Fastclick(document.body)
 
-createApp(App)
+createApp(App as any)
   .use(router)
   .use(lazyLoad, {
     defaultImg: require('./common/image/default.png')
